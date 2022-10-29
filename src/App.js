6f4596c6880d2dd868f2cd2da2a50be1a2077248
dhiya-debbech <dhiyadebbech1996@gmail.com>
2022-10-29 12:01:55 +0100

    todo_list

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@
 import AddTodo from './Components/AddTodo';
 import Navigation from './Components/Navigation';
 import TodoList from './Components/TodoList';
-import { useDispatch } from "react-redux"; 
+
 
 
 function App() {
-  const dispatch = useDispatch(); // Works!
+ 
   return (
     <div className="App">
      <Navigation/>
